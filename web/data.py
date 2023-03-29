82cd94108873a008f8453b72f2bcda1fef11de66
Han Marie <71927533+jaiwon880@users.noreply.github.com>
2023-03-29 19:16:30 +0900

    123

--- web/data.py
+++ web/data.py
@@ -4,7 +4,7 @@ import glob
 
 @st.cache_data
 def read_data():
-    glob_pt = '/jaiwon880/ML_Projiect/web/static/*.csv'
+    glob_pt = '/app/ml_project/SW/review/static/*.csv'
     return pd.concat([pd.read_csv(p) for p in sorted(glob.glob(glob_pt))])  
 
 def get_unique_list(data, index):
